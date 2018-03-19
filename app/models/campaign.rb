3bf33c73a6d996d33c5859905b976b07b6298597
Kadu Simoes <kadu_simoes_1@hotmail.com>
2018-03-18 22:25:26 -0300

    campaign mailer setup

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,14 +1,22 @@
 class Campaign < ApplicationRecord
   include Fae::BaseModelConcern
+
   validates :title, presence: true
   validates :body, presence: true
-
-  has_many :campaign_clients
+  has_many :campaign_clients, dependent: :destroy
   has_many :clients, through: :campaign_clients
+
+  after_save :schedule_emails
 
   def fae_display_field
     title
   end
 
-
+  def schedule_emails
+    Client.find_each do |client| # visto que o processamento será por blocos ao invés de colocar todos os objetos na memória
+    # Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
 end
